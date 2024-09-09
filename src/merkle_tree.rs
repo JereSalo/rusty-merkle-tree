476f7acb3b9638f72f3f970eee528057377d9b30
JereSalo <jeresalo17@gmail.com>
2024-09-09 11:36:26 -0300

    add basic structure merkle tree

---
+++ src/merkle_tree.rs
@@ -0,0 +1,22 @@
+struct MerkleTree{
+    tree: Vec<Vec<String>>,
+}
+
+impl MerkleTree{
+    pub fn build(elements: Vec<String>) -> self{
+        MerkleTree { tree[0] = elements }
+        todo!();
+    }
+
+    pub fn verify(hash: String, proof: Vec<String>){
+        todo!();
+    }
+
+    pub fn gen_proof(){
+        todo!();
+    }
+
+    pub fn add_element(){
+        todo!();
+    }
+}
